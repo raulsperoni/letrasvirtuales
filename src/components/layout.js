590ba11ca121d8fae59e46da46f5d44cda1246a5
raúl <rsperoni@magnesium.coop>
2021-03-01 18:37:51 -0300

    primera aprox

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,27 +24,13 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+    <div className="leading-normal tracking-normal text-white gradient">
+      {/*<Header siteTitle={data.site.siteMetadata?.title || `Title`} />*/}
+      <div>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        <Footer siteTitle={data.site.siteMetadata?.title || `Title`}></Footer>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Hero from "../components/hero"
+import Section1 from "../components/section1"
+import Waves from "../components/waves"
+import Waves2 from "../components/waves2"
+import Section2 from "../components/section2"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Hero siteTitle="El Templo del Futuro" siteBajada="Lalal las kjhdsdkhsk kjhsd jhkjhkj kjhkj hkj hkjhk jkjhkj khlhljhjhkh."/>
+    <Waves fill="#000000"/>
+    <Section1/>
+    <Waves2 fill="#000000"/>
+    <Section2/>
   </Layout>
 )
 
